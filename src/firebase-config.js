d333653fb50b9ed770ff04683ac58b85e62cb35e
aymanfouad22 <afouad@dons.usfca.edu>
2024-12-06 12:51:26 -0800

    update

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,15 +2,16 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'; // Ensure this is imported
 
+
 // Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCWIp3zH_XewDqf-9oB6Kfzb9tQDy5PITE",
-  authDomain: "bookstore-eb8b4.firebaseapp.com",
-  projectId: "bookstore-eb8b4",
-  storageBucket: "bookstore-eb8b4.appspot.com",
-  messagingSenderId: "310735558099",
-  appId: "1:310735558099:web:330593276ddcc8d4150b26",
-  measurementId: "G-LZ4GXPD8PC"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
